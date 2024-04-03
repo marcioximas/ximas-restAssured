188d01e66c5caa2f166b6a7f15f1296652704353
marcioximas <marcioximas@gmail.com>
2024-04-02 22:09:39 -0300

    Implemented Update product

--- src/main/java/stepDefinitions/MyStepdefs.java
+++ src/main/java/stepDefinitions/MyStepdefs.java
@@ -1,5 +1,6 @@
 package stepDefinitions;
 
+import io.cucumber.java.en.And;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
@@ -10,6 +11,8 @@ import io.restassured.response.ResponseBody;
 import io.restassured.specification.RequestSpecification;
 import org.json.JSONObject;
 
+import static io.restassured.RestAssured.given;
+import static io.restassured.RestAssured.sessionId;
 import static org.junit.Assert.*;
 
 public class MyStepdefs {
@@ -24,6 +27,8 @@ public class MyStepdefs {
 
     public ResponseBody body;
 
+    public JSONObject requestParams;
+
     @Given("I have the base url for the api")
     public void iHaveTheBaseUrlForTheApi() {
         RestAssured.baseURI = "https://fakestoreapi.com/";
@@ -32,7 +37,7 @@ public class MyStepdefs {
     @When("I send a GET request to the get all products api")
     public void i_send_a_get_request_to_the_get_all_products_api() {
 
-        httpRequest = RestAssured.given();
+        httpRequest = given();
         response = httpRequest.get("/products");
     }
 
@@ -50,8 +55,8 @@ public class MyStepdefs {
 
     @When("I pass the url of products in the request")
     public void iPassTheUrlOfProductsInTheRequest() {
-    //    httpRequest = RestAssured.given();
-    //    response = httpRequest.get("/products");
+        httpRequest = given();
+        response = httpRequest.get("/products");
 
     }
 
@@ -74,17 +79,66 @@ public class MyStepdefs {
 
     @Given("I hit the url of post product api endpoint")
     public void iHitTheUrlOfPostProductApiEndpoint() {
-      //  RestAssured.baseURL = "http://fakestoreapi.com";
-     //   httpRequest = httpRequest.given();
-      //  JSONObject requestParams = new JSONObject();
-     //   requestParams.put()
+        RestAssured.baseURI = "http://fakestoreapi.com";
+        httpRequest = given();
+        requestParams = new JSONObject();
+
+
 
     }
     @When("I pass the request body of products {string}")
-    public void i_pass_the_request_body_of_products(String string) {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
+    public void i_pass_the_request_body_of_products(String title) {
+        requestParams.put("title",title);
+        requestParams.put("price",23.5);
+        requestParams.put("description","The shoes perfect for your run!!");
+        requestParams.put("image","https://i.pravatar.cc");
+        requestParams.put("category","electronic");
+        httpRequest.body(requestParams.toString());
+        response = httpRequest.post("/products");
+        body = response.getBody();
+
     }
 
 
+    @Then("I receive the response body with id as {string}")
+    public void iReceiveTheResponseBodyWithIdAs(String id) {
+        JsonPath jsonPath = body.jsonPath();
+
+        String s = jsonPath.get("id").toString();
+        assertEquals(id,s);
+
+    }
+
+    @Given("I hit the url of put product api endpoint")
+    public void iHitTheUrlOfPutProductApiEndpoint() {
+        RestAssured.baseURI = "https://fakestoreapi.com/";
+        requestParams = new JSONObject();
+    }
+
+    @When("I pass the url of products in the request with {string}")
+    public void iPassTheUrlOfProductsInTheRequestWith(String productNumber) {
+        httpRequest = given();
+        requestParams.put("title","Test");
+        requestParams.put("price",23.4);
+        requestParams.put("description","The sshoes perfect for your run!!");
+        requestParams.put("image","https://i.pravatar.ccc");
+        requestParams.put("category","electroniccc");
+        httpRequest.body(requestParams.toString());
+        response = httpRequest.put("/products/"+ productNumber);
+        body = response.getBody();
+
+        JsonPath jsonPath = body.jsonPath();
+
+        // s = jsonPath.get("id").toString();
+        System.out.println(response.getStatusLine());
+        System.out.println(body.asString());
+
+
+
+    }
+
+    @And("I pass the request body of put api")
+    public void iPassTheRequestBodyOfPutApi() {
+
+    }
 }
--- src/main/resources/InsertProduct.feature
+++ src/main/resources/InsertProduct.feature
@@ -1,12 +1,21 @@
 Feature: Insert product using POST API
 
-  Scenario Outline: Validate post product API works correctly
+  Scenario Outline: Validate post product API status code works correctly
     Given I hit the url of post product api endpoint
     When I pass the url of products in the request
     And  I pass the request body of products "<title>"
     Then I should get a response code 200
     Examples:
       | title |
-      | Shoes   |
+      | shoes |
+
+  Scenario Outline: Validate post product api response body works correctly
+    Given I hit the url of post product api endpoint
+    When I pass the url of products in the request
+    And  I pass the request body of products "<title>"
+    Then I receive the response body with id as "<id>"
+    Examples:
+      | title | id |
+      | shoes | 21 |
 
 
